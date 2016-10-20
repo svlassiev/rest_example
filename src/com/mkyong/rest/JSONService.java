c5022820d047b0876276e0693d58a8e6f445cb36
Sergey Vlassiev <svlassiev@gmail.com>
2016-10-20 16:15:09 +0200

    Update examples with @CookieParam

--- src/com/mkyong/rest/JSONService.java
+++ src/com/mkyong/rest/JSONService.java
@@ -2,91 +2,96 @@ package com.mkyong.rest;
 
 import javax.servlet.http.HttpServletResponse;
 import javax.ws.rs.*;
-import java.io.IOException;
+import javax.ws.rs.core.Response;
+import java.util.List;
 
 
 @Path("/json/metallica")
 public class JSONService {
-//
+
 //	@GET
-//	@Path("/get")
+//	@Path("/getJson")
 //	@Produces(MediaType.APPLICATION_JSON)
 //	public Track getTrackInJSON() {
-//
 //		Track track = new Track();
 //		track.setTitle("Enter Sandman");
 //		track.setSinger("Metallica");
-//
 //		return track;
+//	}
 //
+//	@GET
+//	@Path("/getXML")
+//	@Produces(MediaType.APPLICATION_XML)
+//	public Track getTrackInXML() {
+//		Track track = new Track();
+//		track.setTitle("Enter Sandman");
+//		track.setSinger("Metallica");
+//		return track;
 //	}
 //
 //	@POST
 //	@Path("/post")
 //	@Consumes(MediaType.APPLICATION_JSON)
 //	public Response createTrackInJSON(Track track) {
-//
 //		String result = "Track saved : " + track;
 //		return Response.status(201).entity(result).build();
-//
 //	}
-//
-//	@GET
-//	@Path("/print{param}")
-//	@Produces(MediaType.TEXT_PLAIN)
-//	public String getAndPrint(@PathParam("param") String param) {
-//		return param;
-//	}
-//
-//	@GET
-//	@Path("/query")
-//	public Response getUsers(
-//			@QueryParam("from") int from,
-//			@QueryParam("to") int to,
-//			@QueryParam("orderBy") List<String> orderBy) {
-//
-//		return Response
-//				.status(200)
-//				.entity("getUsers is called, from : " + from + ", to : " + to
-//						+ ", orderBy" + orderBy.toString()).build();
-//
-//	}
-//
 
 	private HttpServletResponse response;
 
 	@GET
+	@Path("/query")
+	public Response getUsers(
+			@QueryParam("from") int from,
+			@QueryParam("to") int to,
+			@QueryParam("orderBy") List<String> orderBy) {
+		final String entity = "getUsers is called, from : " + from + ", to : " + to + ", orderBy" + orderBy.toString();
+		try { response.getWriter().print(entity); } catch (Throwable t) {} // Cross-site Scripting
+		return Response.status(200).entity(entity).build(); // Cross-site Scripting
+	}
+
+	@GET
 	@Path("/vulnqueryandheader")
-	public String printVulnAndHeader(@QueryParam("vulnparam") String from, @HeaderParam("vulnheader") String header, String param) throws IOException {
-		response.getWriter().print(from);
-		response.getWriter().print(header);
-		response.getWriter().print(param);
+	public String printVulnAndHeader(@QueryParam("vulnparam") String from, @HeaderParam("vulnheader") String header, String param) {
+		try { response.getWriter().print(from); } catch (Throwable t) {} // Cross-site Scripting
+		try { response.getWriter().print(header); } catch (Throwable t) {} // Cross-site Scripting
+		try { response.getWriter().print(param); } catch (Throwable t) {} // Cross-site Scripting
 		return from;
+	}
+
+	@GET
+	@Path("/printPath{param}")
+	public String getAndPrint(@PathParam("param") String param) {
+		try { response.getWriter().print(param); } catch (Throwable t) {} // Cross-site Scripting
+		return param; // Cross-site Scripting
 	}
 
 	@GET
 	@Path("/vulnquery")
-	public String printVuln(@QueryParam("vuln") String from) throws IOException {
-		response.getWriter().print(from);
-		return from;
+	public String printVuln(@QueryParam("vuln") String from) {
+		try { response.getWriter().print(from); } catch (Throwable t) {} // Cross-site Scripting
+		return from; // Cross-site Scripting
 	}
+//
+//    @GET
+//    @Encoded
+//    @Path("/vulnencodedquery")
+//    public String printVulnEncoded(@QueryParam("vuln") String from) {
+//        try { response.getWriter().print(from); } catch (Throwable t) {} // Cross-site Scripting
+//        return from; // Cross-site Scripting
+//    }
 
 	@GET
 	@Path("/vulnheader")
-	public String printHeaderVuln(@HeaderParam("vuln") String header) throws IOException {
-		response.getWriter().print(header);
-		return header;
+	public String printHeaderVuln(@HeaderParam("vuln") String header) {
+		try { response.getWriter().print(header); } catch (Throwable t) {} // Cross-site Scripting
+		return header; // Cross-site Scripting
 	}
-//
-//	@GET
-//	@Path("/vulnpath{param}")
-//	public String printPathVuln(@PathParam("param") String from) throws IOException {
-//		response.getWriter().print(from);
-//		return from;
-//	}
-//
-//	public String print(String from) throws IOException {
-//		response.getWriter().print(from);
-//		return from;
-//	}
+
+    @GET
+    @Path("/vulncookie")
+    public String printCookieVuln(@CookieParam("vuln") String cookie) {
+        try { response.getWriter().print(cookie); } catch (Throwable t) {} // Cross-site Scripting
+        return cookie; // Cross-site Scripting
+    }
 }
